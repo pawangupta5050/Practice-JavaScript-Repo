03f1be9459526381df4c4e99cbb8fe051b0f1346
pawangupta5050 <85182279+pawangupta5050@users.noreply.github.com>
2023-01-10 19:34:52 +0530

    Added some topics

--- JavaScript/class.js
+++ JavaScript/class.js
@@ -1,4 +1,5 @@
 // new keyword - it creates an empty object i.e this, and takes the reference and sets it into __proto__ or [[prototype]]
+// new keyword - it creates an Instance of an Object or a function for which it is called 
 
 const newFunction = function(name, age) {
     this.name = name;
@@ -11,4 +12,35 @@ newFunction.prototype.myname = function() {
 
 const userPawan =  new newFunction("Pawan", 21)
 const userAkash =  newFunction("Akash", 18)
-console.log(userPawan.myname())
+console.log(userPawan.myname)
+
+// Class 
+ 
+class OnlineStore {
+    constructor(custName, custType, custJoining) { // Instance of a class
+        this.custName = custName;
+        this.custType = custType;
+        this.custJoining = custJoining;
+    }
+
+    isPremium() {
+        return this.custType === "Premium";
+    }
+}
+
+const cust1 = new OnlineStore("Pawan", "Premium", "12/1/2023")
+
+console.log(cust1.isPremium())
+
+class Customer extends OnlineStore { // Javascript inheritence 
+    constructor(custName, custType, custJoining, custMobile){
+        super(custName, custType, custJoining)
+        this.custMobile = custMobile;
+    }
+
+    custMobile() {
+        return this.custMobile;
+    }
+}
+
+const customer2 = new Customer("Akash")
