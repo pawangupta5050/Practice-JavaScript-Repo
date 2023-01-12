5e7100b53157f162bbd1749b79a4c06bc876a7c7
pawangupta5050 <85182279+pawangupta5050@users.noreply.github.com>
2023-01-12 22:48:28 +0530

    Added

---
+++ JavaScript/DOM/dom.js
@@ -0,0 +1,8 @@
+// getElementById - Get any element through ID
+const myName = 'Pawan Gupta'
+
+const heading1 = document.getElementById('heading')
+
+heading1.innerHTML = `Hello ${myName}`
+
+console.log(heading1.innerHTML)
