ec8a9d227a6691707f29387790b53612fc4c3769
pawangupta5050 <85182279+pawangupta5050@users.noreply.github.com>
2023-01-23 23:02:06 +0530

    Changes

--- JavaScript/resume-builder/script.js
+++ JavaScript/resume-builder/script.js
@@ -1,8 +1,10 @@
 const download = () => {
-    let resume = document.getElementById("resume").innerHTML;
+    const resume = document.getElementById("resume").innerHTML;
+    const style = document.getElementsByTagName('link')
+    console.log(style)
     let a = window.open('', '', 'height=500, width=500');
     a.document.write('<html><head><title>Resume</title>');
-    a.document.write('<link rel="stylesheet" href="css/midday_receipt.css" type="text/css" />');
+    a.document.write('<link rel="stylesheet" href="./style.css" type="text/css" />');
     a.document.write('</head>');
     a.document.write('<body > <h1>Div contents are <br>');
     a.document.write(resume);
